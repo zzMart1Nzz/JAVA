24845b92db7dd71432d939d39a6e5a8d4df56e2e
zzMart1Nzz <b.martin.ulanga@gmail.com>
2025-02-07 12:08:10 +0100

    insert eta update batzuk sortuta

--- 2Erronka/src/eragiketak/aldaketaLangileak.java
+++ 2Erronka/src/eragiketak/aldaketaLangileak.java
@@ -5,11 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import java.awt.Color;
 
-public class aldaketaProduktuak extends JFrame {
+public class aldaketaLangileak extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -21,7 +18,7 @@ public class aldaketaProduktuak extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					aldaketaProduktuak frame = new aldaketaProduktuak();
+					aldaketaLangileak frame = new aldaketaLangileak();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -33,19 +30,13 @@ public class aldaketaProduktuak extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public aldaketaProduktuak() {
+	public aldaketaLangileak() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1000, 490);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\UTech java fondoa 1(2).png"));
-		lblNewLabel.setBounds(0, 0, 988, 445);
-		contentPane.add(lblNewLabel);
 	}
+
 }
